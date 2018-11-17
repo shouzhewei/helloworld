3b14b1c14f47edd54645cfe414103d501d0f437a
szw <szw@szwdeMacBook-Pro.local>
2018-11-17 13:16:23 +0800

    Modify equation_puzzle.cpp

--- equation_puzzle.cpp
+++ equation_puzzle.cpp
@@ -1,48 +1,60 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <iostream>
-using namespace std;
 
 const int MAXN = 129;
 
-char a[100];
-char b[100];
-char c[100];
+char a[102];
+char b[102];
+char c[102];
 char variables[129];
 char selected_variables[129];
 
 void format_print(char* vmap)
 {
+    int cnt = 0;
+    char output[102];
+
     for(int i=0; i<strlen(a); i++)
-        printf("%d", vmap[a[i]]);
-    printf(" * ");
+        output[cnt++] = vmap[a[i]] + '0';
+
+    output[cnt++] = ' ';
+    output[cnt++] = '*';
+    output[cnt++] = ' ';
+
     for(int i=0; i<strlen(b); i++)
-        printf("%d", vmap[b[i]]);
-    printf(" = ");
+        output[cnt++] = vmap[b[i]] + '0';
+
+    output[cnt++] = ' ';
+    output[cnt++] = '=';
+    output[cnt++] = ' ';
+
     for(int i=0; i<strlen(c); i++)
-        printf("%d", vmap[c[i]]);
-    puts("");
+        output[cnt++] = vmap[c[i]] + '0';
+
+    output[cnt] = '\0';
+    printf("%s\n", output);
 }
 
-void calc(char* vmap, int cnt)
+int calc_value(char* str, char* vmap)
 {
-    int va=0;
-    for(int i=0; i<strlen(a); i++)
-        va = va*10 + vmap[a[i]];
+    int ret = 0;
 
-    int vb=0;
-    for(int i=0; i<strlen(b); i++)
-        vb = vb*10 + vmap[b[i]];
+    for(int i=0; i<strlen(str); i++)
+        ret = ret*10 + vmap[str[i]];
 
-    int vc=0;
-    for(int i=0; i<strlen(c); i++)
-        vc = vc*10 + vmap[c[i]];
+    return ret;
+}
 
-    if(va * vb == vc)
+void calc(char* vmap)
+{
+    int va = calc_value(a, vmap);
+    int vb = calc_value(b, vmap);
+    int vc = calc_value(c, vmap);
+
+    if((va * vb == vc) && (vmap[a[0]] != 0 && vmap[b[0]] != 0 && vmap[c[0]] != 0))
     {
-        if(vmap[a[0]] != 0 && vmap[b[0]] != 0 && vmap[c[0]] != 0)
-            format_print(vmap);
+        format_print(vmap);
     }
 }
 
@@ -62,7 +74,7 @@ void vloop(int idx, char* vmap, int* used, int cnt)
 
         if(idx == cnt-1)
         {
-            calc(vmap, cnt);
+            calc(vmap);
         }
         else
         {
@@ -72,26 +84,21 @@ void vloop(int idx, char* vmap, int* used, int cnt)
     }
 }
 
-int main(int args, char** argv)
+int main(int, char** argv)
 {
     freopen(argv[1], "r", stdin);
-    //freopen("slyar.out", "w", stdout);
 
-    while(scanf("%s * %s = %s", a, b, c)!=EOF)
+    while(scanf("%100s * %100s = %100s", a, b, c)!=EOF)
     {
-        //printf("%s %s %s\n", a, b, c);
         memset(variables, 0, sizeof(variables));
-        for (int i=0; a[i]!='\0'; i++)
+        for (int i=0; a[i]!='\0' || b[i]!='\0' || c[i]!='\0'; i++)
         {
-            variables[a[i]] = 1;
-        }
-        for (int i=0; b[i]!='\0'; i++)
-        {
-            variables[b[i]] = 1;
-        }
-        for (int i=0; c[i]!='\0'; i++)
-        {
-            variables[c[i]] = 1;
+            if(a[i]!='\0')
+                variables[a[i]] = 1;
+            if(b[i]!='\0')
+                variables[b[i]] = 1;
+            if(c[i]!='\0')
+                variables[c[i]] = 1;
         }
 
         int var_cnt=0;
@@ -110,7 +117,6 @@ int main(int args, char** argv)
     }
 
     fclose(stdin);
-    //fclose(stdout);
     return 0;
 }
 
