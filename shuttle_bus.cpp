3622bcf235c2909af6588705fae78b5ba0837ad5
szw <szw@szwdeMacBook-Pro.local>
2018-11-17 13:39:37 +0800

    Modify shuttle_bus.cpp

--- shuttle_bus.cpp
+++ shuttle_bus.cpp
@@ -4,58 +4,34 @@
 #include <iostream>
 using namespace std;
 
-const int MAXN = 102;
-
-void dp(int m, int n, int map[][MAXN])
-{
-    int val[MAXN][MAXN];
-    memset(val, 0, sizeof(val));
-
-    for(int i=0; i<n; i++)
-        val[i][0] = map[i][0];
-    for(int i=0; i<m; i++)
-        val[0][i] = map[0][i];
-
-    for(int i=1; i<=n; i++)
-        for(int j=1; j<=m; j++)
-        {
-            val[i][j] = max(val[i-1][j] + map[i][j], val[i][j-1] + map[i][j]);
-        }
-
-    /*
-    for(int i=0; i<=n; i++)
-    {
-        for(int j=0; j<=m; j++)
-            printf("%d ", val[i][j]);
-        puts("");
-    }
-    */
-
-    printf("%d\n", val[n][m]);
-}
+const int MAXN = 105;
 
 int main(int args, char** argv)
 {
     freopen(argv[1], "r", stdin);
-    //freopen("output", "w", stdout);
 
     int n, m;
     int map[MAXN][MAXN];
     while(scanf("%d, %d", &m, &n)!=EOF)
     {
+        memset(map, 0, sizeof(map));
+
         for(int i=1; i<=n; i++)
         {
-            int j;
+            int j, val;
             for(j=1; j<m; j++)
-                scanf("%d, ", &map[i][j]);
-            scanf("%d", &map[i][j]);
+            {
+                scanf("%d,", &val);
+                map[i][j] = max(map[i-1][j] + val, map[i][j-1] + val);
+            }
+            scanf("%d", &val);
+            map[i][j] = max(map[i-1][j] + val, map[i][j-1] + val);
         }
 
-        dp(m, n, map);
+        printf("%d\n", map[n][m]);
     }
 
     fclose(stdin);
-    //fclose(stdout);
     return 0;
 }
 
